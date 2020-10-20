8f9f0e086fbfba6020ad2408f754724d2a011900
Emre Cebeci <kerata_06_fb@hotmail.com>
2020-10-20 16:43:50 +0300

    v1.1

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+"""
+@ Sheltered Save Editor
+@ File: main.py
+@ Version: 1.1
+@ Author: Emre Cebeci
+@ GitHub: https://github.com/emreyigitcbc/
+"""
+# pip install eel
 import eel
-import xml.etree.ElementTree as ET
-eel.init('web')
-eel.start('main.html')
 
-@eel.expose
-def print_table_py(veri):
-    c = open("cache_file", "w")
-    c.write(veri)
+# Select "web" folder as root
+eel.init('web')
+# Start main.html as index
+eel.start('main.html')
---
+++ web/item_names.js
@@ -0,0 +1,15 @@
+/*
+ * SHELTERED SAVE EDITOR
+ * File Name: item_names.js
+ * Version: 1.1
+ * Author: Emre Cebeci
+ * GitHub: https://github.com/emreyigitcbc/
+ */
+
+ /*
+  If you want to add names, there is a example:
+  {"id": "name"}
+  name_list = '{"51": "Bulletproof Vest","52": "something else","53": "something"}';
+  IT MUST BE SINGLE LINED!
+*/
+name_list = '{"51": "Bulletproof Vest"}';
